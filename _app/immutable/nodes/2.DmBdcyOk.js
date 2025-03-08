9f402367c32441ca76dd381530d1b8d2b4b36225
ashen114 <1215772764@qq.com>
2025-03-08 18:17:12 +0800

    Site updated: 2025-3-8 18:17:10

---
+++ _app/immutable/nodes/2.DmBdcyOk.js
@@ -0,0 +1,19 @@
+import{t as n,a as s}from"../chunks/CB6gApr2.js";import"../chunks/a-rk7CLH.js";import{f as i,s as r}from"../chunks/BpKFZ6QP.js";import{S as m}from"../chunks/CQEB1Mx_.js";var l=n("<!> <header></header>",1);function d(t){console.log(`%c
+    
+
+     █████╗ ███████╗██╗  ██╗███████╗███╗   ██╗
+    ██╔══██╗██╔════╝██║  ██║██╔════╝████╗  ██║
+    ███████║███████╗███████║█████╗  ██╔██╗ ██║
+    ██╔══██║╚════██║██╔══██║██╔══╝  ██║╚██╗██║
+    ██║  ██║███████║██║  ██║███████╗██║ ╚████║
+    ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝
+    
+
+
+    Resume: https://ashen114.github.io/resume
+
+    Blog: https://github.com/ashen114/blog
+
+    Email: 1215772764@qq.com / guxs2016@gmail.com
+
+    `,"font-family: monospace; color: #00C9FF;"),console.log("</div>");var o=l(),e=i(o);m(e,{title:"阿沈是也",description:"ashen114",canonical:"https://ashen114.github.io",keywords:"blog"});var a=r(e,2);a.textContent="阿沈是也：<div>",s(t,o)}export{d as component};
